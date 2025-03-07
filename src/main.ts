63b7c044af7e42e6099225ef169b4ed06f7eb384
KarenHoil-Dapper <k.hoil@dappertechnologies.com>
2025-03-07 17:47:00 -0500

    Karen Hoil

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
 import './assets/main.css'
 
+//PRIMEVUE
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css' // Iconos
+
+//IMPORTS COMPONENTES PRIMEVUE
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
+import MultiSelect from 'primevue/multiselect';
+import Textarea from 'primevue/textarea';
+import FloatLabel from 'primevue/floatlabel';
+import DatePicker from 'primevue/datepicker';
+import Chip from 'primevue/chip';
+import Card from 'primevue/card';
+import Tag from 'primevue/tag';
+import Password from 'primevue/password';
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
+import Tooltip from 'primevue/tooltip';
+
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -7,6 +32,37 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.component('Button', Button)
+app.component('Dialog', Dialog)
+app.component('InputText', InputText)
+app.component('MultiSelect', MultiSelect)
+app.component('Textarea', Textarea)
+app.component('FloatLabel', FloatLabel)
+app.component('DatePicker', DatePicker)
+app.component('Chip', Chip)
+app.component('Card', Card)
+app.component('Tag', Tag)
+app.component('Password', Password)
+
+app.component('Tabs', Tabs)
+app.component('TabList', TabList)
+app.component('Tab', Tab)
+app.component('TabPanels', TabPanels)
+app.component('TabPanel', TabPanel)
+app.directive('tooltip', Tooltip);
+
+app.use(PrimeVue, {
+  // Default theme configuration
+  theme: {
+      preset: Aura,
+      options: {
+          prefix: 'p',
+          darkModeSelector: null,
+          cssLayer: true
+      }
+  }
+});
 
 app.use(createPinia())
 app.use(router)
