4c5f573bb213f080ab15fda9b518d680ac26aba6
eduardo87DR <spixerspi@gmail.com>
2025-03-14 12:39:07 -0500

    Layouts de Header y footer para paginas, vista de perfil

---
+++ src/components/LayoutComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layout">
+    <HeaderComponent />
+    <main class="main-content">
+      <slot></slot>
+    </main>
+    <FooterComponent />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import HeaderComponent from './HeaderComponent.vue';
+import FooterComponent from './FooterComponent.vue';
+</script>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
+  padding: 20px;
+  overflow-y: auto;
+}
+</style>
