16109753e45d5a2c578627acaea33b90497fd651
KarenHoil-Dapper <k.hoil@dappertechnologies.com>
2025-03-14 20:21:48 -0500

    correction

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
-import type { AuthToken, RegisterUser, UserLogin } from "@/interfaces";
+import type { AuthToken } from "@/interfaces/AuthToken";
+import type { RegisterUser } from "@/interfaces/RegisterUser";
+import type { UserLogin } from "@/interfaces/UserLogin";
 
 
 const API_URL = "http://localhost:7027/api/auth";
@@ -11,5 +13,6 @@ export  function login(user: UserLogin): Promise<AuthToken> {
 export  function register(user: RegisterUser): Promise<void> {
   return axios.post(`${API_URL}/register`, user).then((response) => response.data);
 }
-export { UserLogin };
+
+// export { UserLogin };
 
