7523b0c6cde4c6067156728e0be6c4600cec3011
Ethan Baro <ethanbarea@gmail.com>
2025-03-14 22:52:00 -0500

    Home visitantes hecho

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
 import UserProfile from '@/views/UserProfile.vue'
+import HomeVisitor from '@/views/HomeVisitor.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,7 +10,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeVisitor,
       // meta: { requiresAuth: true },
     },
     {
