d2b55d9807709e867214651f5a483b4122727f32
Ethan Baro <ethanbarea@gmail.com>
2025-03-20 13:49:06 -0500

    layout home visitante y view usuario (en progreso)

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,39 +3,45 @@ import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
 import UserProfile from '@/views/UserProfile.vue'
 import HomeVisitor from '@/views/HomeVisitor.vue'
+import ViewHomeUsuario from '@/views/ViewHomeUsuario.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeVisitor,
       // meta: { requiresAuth: true },
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/register',
-      name: 'register',
+      path: "/register",
+      name: "register",
       component: RegisterView,
       meta: { requiresAuth: false },
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: LoginView,
       meta: { requiresAuth: false },
     },
     {
-      path: '/user-profile',
-      name: 'user-profile',
+      path: "/user-profile",
+      name: "user-profile",
       component: UserProfile,
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: ViewHomeUsuario,
     }
   ],
-})
+});
 
 export default router
--- src/views/HomeVisitor.vue
+++ src/views/HomeVisitor.vue
@@ -1,70 +1,57 @@
 <script setup lang="ts">
-import FooterComponent from '@/components/FooterComponent.vue';
 import IconTextBubble from '@/components/icons/IconTextBubble.vue';
 import IconThumbsUp from '@/components/icons/IconThumbsUp.vue';
 import IconTrophy from '@/components/icons/IconTrophy.vue';
-import HeaderVisitante from '@/components/HeaderVisitante.vue';
+import LayoutVisitante from '../layouts/LayoutVisitante.vue';
 </script>
 
 <template>
-    <div class="flex flex-col">
-        <!-- Header fijo -->
-        <HeaderVisitante />
-
-        <!-- Contenido desplazable -->
-        <main class="flex-1 overflow-y-auto mt-16">
-            <!-- Sección principal -->
-            <section class="h-[80%] justify-center items-center bg-gradient-to-r from-gray-900 to-black text-white text-center px-16">
-                <div class="grid grid-flow-col grid-rows-2 gap-8 pt-6">
-                    <div class="px-8 row-span-3 flex justify-center items-center">
-                        <img src="../assets/images/login/image1.png" alt="Mascota" class="" />
-                    </div>
-                    <div class="flex justify-center items-center pl-8">
-                        <h2 class="tracking-wide text-8xl text-left font-semibold m-4 mt-8">Donde las
-                            ideas toman vida</h2>
-                    </div>
-                    <div class="text-4xl text-left pl-8">
-                        <h2 class="tracking-wide m-4 mt-8">El hogar de los desarrolladores
-                            de videojuegos en
-                            Latinoamérica</h2>
-                        <div class="grid place-items-center">
-                            <button
-                                class="flex justify-center mt-8 bg-purple-600 px-6 py-3 rounded-lg hover:bg-purple-700 transition-colors">Regístrate
-                                hoy</button>
-                        </div>
+    <LayoutVisitante>
+        <!-- Sección principal -->
+        <section
+            class="h-[80%] justify-center items-center bg-gradient-to-r from-gray-900 to-black text-white text-center px-16">
+            <div class="grid grid-flow-col grid-rows-2 gap-8 pt-6">
+                <div class="px-8 row-span-3 flex justify-center items-center">
+                    <img src="../assets/images/login/image1.png" alt="Mascota" class="" />
+                </div>
+                <div class="flex justify-center items-center pl-8">
+                    <h2 class="tracking-wide text-8xl text-left font-semibold m-4 mt-8">Donde las
+                        ideas toman vida</h2>
+                </div>
+                <div class="text-4xl text-left pl-8">
+                    <h2 class="tracking-wide m-4 mt-8">El hogar de los desarrolladores
+                        de videojuegos en
+                        Latinoamérica</h2>
+                    <div class="grid place-items-center">
+                        <button
+                            class="flex justify-center mt-8 bg-purple-600 px-6 py-3 rounded-lg hover:bg-purple-700 transition-colors">Regístrate
+                            hoy</button>
                     </div>
                 </div>
-                <div>
-                </div>
-            </section>
+            </div>
+        </section>
 
-
-            <!-- Mas contenido -->
-            <section class="p-8 mt-8 grid grid-cols-1 md:grid-cols-3 bg-gray-50 gap-6 text-black">
-                <div class="text-center flex flex-col justify-center items-center">
-                    <h3 class="font-bold text-2xl">Comparte tus logros</h3>
-                    <IconTrophy class="size-12 my-4" />
-                    <p class="mt-2 text-lg">Publica tus avances y proyectos terminados. Celebra tus éxitos y recibe el
-                        reconocimiento de la comunidad.</p>
-                </div>
-                <div class="text-center flex flex-col justify-center items-center border-x-2">
-                    <h3 class="font-bold text-2xl">Conecta con talentos</h3>
-                    <IconTextBubble class="size-12 my-4" />
-                    <p class="mt-2 text-lg">Conecta con desarrolladores y diseñadores. Descubre perfiles inspiradores y
-                        colabora en proyectos.</p>
-                </div>
-                <div class="text-center flex flex-col justify-center items-center">
-                    <h3 class="font-bold text-2xl">Interactúa y crece</h3>
-                    <IconThumbsUp class="size-12 my-4" />
-                    <p class="mt-2 text-lg">Dale like y comenta publicaciones. Intercambia ideas y construye relaciones
-                        en
-                        la comunidad.</p>
-                </div>
-            </section>
-
-        </main>
-        <!-- Footer -->
-    </div>
-    <FooterComponent />
-
+        <!-- Más contenido -->
+        <section class="p-8 mt-8 grid grid-cols-1 md:grid-cols-3 bg-gray-50 gap-6 text-black">
+            <div class="text-center flex flex-col justify-center items-center">
+                <h3 class="font-bold text-2xl">Comparte tus logros</h3>
+                <IconTrophy class="size-12 my-4" />
+                <p class="mt-2 text-lg">Publica tus avances y proyectos terminados. Celebra tus éxitos y recibe el
+                    reconocimiento de la comunidad.</p>
+            </div>
+            <div class="text-center flex flex-col justify-center items-center border-x-2">
+                <h3 class="font-bold text-2xl">Conecta con talentos</h3>
+                <IconTextBubble class="size-12 my-4" />
+                <p class="mt-2 text-lg">Conecta con desarrolladores y diseñadores. Descubre perfiles inspiradores y
+                    colabora en proyectos.</p>
+            </div>
+            <div class="text-center flex flex-col justify-center items-center">
+                <h3 class="font-bold text-2xl">Interactúa y crece</h3>
+                <IconThumbsUp class="size-12 my-4" />
+                <p class="mt-2 text-lg">Dale like y comenta publicaciones. Intercambia ideas y construye relaciones
+                    en
+                    la comunidad.</p>
+            </div>
+        </section>
+    </LayoutVisitante>
 </template>
