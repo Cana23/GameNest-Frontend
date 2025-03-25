c4431a64a2bcf2a848b540ecc4d5845bcf139b05
KarenHoil-Dapper <k.hoil@dappertechnologies.com>
2025-03-24 20:30:24 -0500

    logout

--- src/components/NavPorfileComponent.vue
+++ src/components/NavPorfileComponent.vue
@@ -17,7 +17,7 @@
                         </li>
                         <li class="flex items-center gap-2 px-4 py-3 hover:bg-gray-200 cursor-pointer rounded-border">
                             <div>
-                                <span class="font-medium">Editar perfil</span>
+                                <span class="font-medium"><RouterLink to="/user-profile">Editar perfil</RouterLink> </span>
                             </div>
                         </li>
                         <li class="flex items-center gap-2 px-4 py-3 hover:bg-gray-200 cursor-pointer rounded-border" @click="visible = true">
@@ -35,20 +35,29 @@
   <Dialog :draggable="false" v-model:visible="visible" modal header="Cerrar sesión" :style="{ width: '25rem', backgroundColor:'white', color: 'gray', padding: '10px 20px' }">
             <p class="text-gray-600">¿Estás seguro de que deseas cerrar la sesión?</p>
             <div class="flex justify-end gap-4">
-                <button type="button" @click="visible = false" class="py-2 px-4 bg-red-500 text-white rounded-xl hover:bg-red-700 cursor-pointer">Si</button>
+                <button type="button"@click="handleLogout" class="py-2 px-4 bg-red-500 text-white rounded-xl hover:bg-red-700 cursor-pointer">Si</button>
                 <button type="button" @click="visible = false" class="py-2 px-4 text-purple-600 rounded-xl hover:bg-purple-200 cursor-pointer">No</button>
             </div>
         </Dialog>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/authStore';
 
+const router = useRouter();
+const authStore = useAuthStore();
 const visible = ref(false);
 
 const op = ref();
 const toggle = (event: any) => {
     op.value.toggle(event);
 }
+
+const handleLogout = () => {
+  authStore.logout(); // Llama al método logout del store
+  router.push({ name: 'login' });
+};
 
 </script>
 
